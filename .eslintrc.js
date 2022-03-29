16a067f8d87b06e4b556d1a2fc6fea0872d47270
slienceWhale <842766770@qq.com>
2022-03-29 15:09:12 +0800

    项目初始化-调整初始目录

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,16 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    // '@vue/standard'
   ],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quotes': 'off',
+    'semi': 'off',
+    'comma-dangle': 'off'
   }
 }
